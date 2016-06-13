e1faa5a50fb914482d4fe9d822e4f5995d1e7514
Steve Dunstan <steve_dunstan@yahoo.com>
2016-06-12 19:18:39 -0700

    Added permissions check

--- app/src/main/java/com/twominuteplays/LineActivity.java
+++ app/src/main/java/com/twominuteplays/LineActivity.java
@@ -1,17 +1,25 @@
 package com.twominuteplays;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.twominuteplays.db.FirebaseStuff;
+import com.twominuteplays.db.ShareUtility;
 import com.twominuteplays.model.Line;
 import com.twominuteplays.model.Movie;
 import com.twominuteplays.model.MovieState;
@@ -29,6 +37,7 @@ public class LineActivity extends BaseActivity {
     private static final String TAG = LineActivity.class.getName();
 
     private static final int LINE_RECORDED = 31415;
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 1;
     private Movie mMovie = null;
     private Part mPart = null;
     private Line mCurrentLine = null;
@@ -96,8 +105,58 @@ public class LineActivity extends BaseActivity {
 
     }
 
-    private class RecordVideoClickHandler implements Button.OnClickListener {
+    private boolean cameraPermissionCheck() {
+        boolean ok = false;
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.CAMERA);
 
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                showCameraPermissionsDialog();
+            }
+            else {
+                // Request permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        CAMERA_PERMISSION_REQUEST_CODE);
+            }
+        }
+        else {
+            ok = true;
+        }
+
+
+        return ok;
+    }
+
+    private void recordLine() {
+        if (mCurrentLine == null)
+            return;
+        Intent cameraIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        // EXTRA_OUTPUT, EXTRA_VIDEO_QUALITY, EXTRA_SIZE_LIMIT, EXTRA_DURATION_LIMIT
+        cameraIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 20);
+        StringBuilder fileName = new StringBuilder(mCurrentLine.getId());
+        fileName.append("-").append(new Date().getTime()).append(".mp4");
+        mOutputFile = new File(getMediaDirectory(), fileName.toString());
+        Uri outputFileUri = Uri.fromFile(mOutputFile);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+        cameraIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+        startActivityForResult(cameraIntent, LINE_RECORDED);
+
+    }
+
+    private File getMediaDirectory() {
+        File dir = new File(getExternalFilesDir(Environment.DIRECTORY_MOVIES), "2mp");
+        if (!dir.exists()) {
+            if (! dir.mkdirs() ) {
+                Log.e(TAG, "Could not create directory " + dir.toString());
+                // TODO: fallback
+            }
+        }
+        return dir;
+    }
+
+    private class RecordVideoClickHandler implements Button.OnClickListener {
         private final Line line;
 
         public RecordVideoClickHandler(Line line) {
@@ -106,34 +165,12 @@ public class LineActivity extends BaseActivity {
 
         @Override
         public void onClick(View view) {
-            Intent cameraIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-            // EXTRA_OUTPUT, EXTRA_VIDEO_QUALITY, EXTRA_SIZE_LIMIT, EXTRA_DURATION_LIMIT
-            cameraIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 20);
-            StringBuilder fileName = new StringBuilder(line.getId());
-            fileName.append("-").append(new Date().getTime()).append(".mp4");
-            mOutputFile = new File(getMediaDirectory(), fileName.toString());
-//            Uri outputFileUri = FileProvider.getUriForFile(LineActivity.this, "com.twominuteplays", mOutputFile);
-            Uri outputFileUri = Uri.fromFile(mOutputFile);
-//            Log.d(TAG, "Creating movie file " + outputFileUri.toString());
-//            cameraIntent.setClipData(ClipData.newRawUri("line", outputFileUri));
-            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-            cameraIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
-            mCurrentLine = line;
             mCurrentButton = (Button) view;
-            startActivityForResult(cameraIntent, LINE_RECORDED);
+            mCurrentLine = line;
+            if (!cameraPermissionCheck())
+                return;
+            recordLine();
         }
-
-        private File getMediaDirectory() {
-            File dir = new File(getExternalFilesDir(Environment.DIRECTORY_MOVIES), "2mp");
-            if (!dir.exists()) {
-                if (! dir.mkdirs() ) {
-                    Log.e(TAG, "Could not create directory " + dir.toString());
-                    // TODO: fallback
-                }
-            }
-            return dir;
-        }
-
     }
 
     @Override
@@ -202,6 +239,7 @@ public class LineActivity extends BaseActivity {
         if (mPart.isRecorded()) {
             if (mMovie.getState() == MovieState.RECORDING_STARTED) {
                 mMovie = mMovie.state.recorded(mMovie);
+                ShareUtility.shareMovie(this, mMovie);
             }
 
             if (mMovie.getState() == MovieState.CONTRIBUTE) {
@@ -212,4 +250,37 @@ public class LineActivity extends BaseActivity {
         }
     }
 
+    private void showCameraPermissionsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("To record your part, you need to use your device's camera. Allow Two Minute Plays to access the camera?");
+        builder.setTitle("Camera Permissions Check");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int ID) {
+                cameraPermissionCheck();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // ?
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    recordLine();
+                }
+                break;
+            default:
+                break;
+        }
+    }
 }
